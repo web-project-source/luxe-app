0cf15abd5054816a624431a4b2b8142439c1defe
web-project-source <66437816+web-project-source@users.noreply.github.com>
2021-03-25 14:28:03 +0000

    Update OrderController.php

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -51,7 +51,7 @@ class OrderController extends Controller
      $orders= DB::table('orders_view')->get();
      }
      else {
-     $orders= DB::table('orders_view')->where('userId', Auth::user()->id)->get();
+     $orders= DB::table('orders_view')->where('userName', Auth::user()->name)->get();
      }    
      return view('vieworders',['orders' => $orders]);
     }
